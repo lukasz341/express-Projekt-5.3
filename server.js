edac32ebdd7fef2904dee19f76e709fe4cd09876
lukasz341 <lukasz341@tlen.pl>
2017-12-21 21:38:17 +0100

    dodanie zapisu do pliku

--- server.js
+++ server.js
@@ -2,24 +2,35 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var stringifyFile;
-//app.use(bodyParser.json());
+var bodyParser= require('body-parser');
+app.use(bodyParser.json());
 
 app.get('/getNote', function(req, res)
 {
     
         fs.readFile('./test.json', 'utf8', function(err, data) {
             if (err) throw err;
-            stringifyFile = data
+            stringifyFile = data;
             res.send(data);
         })
-
-    
+        
+   
 }
 
 );
 
-var server = app.listen(3000,function(){
-    console.log('Aplikacja nasłuchuje na http://localhost:3000')
+
+app.post('/updateNote/:note', function (req, res) {   
+    stringifyFile = stringifyFile + req.params.note;
+   fs.writeFile('./test.json', stringifyFile, function(err) {
+    if (err) throw err;
+    console.log('file updated');
+})
+
+});
+
+var server = app.listen(8000,function(){
+    console.log('Aplikacja nasłuchuje na http://localhost:8000');
 }
 
 );
